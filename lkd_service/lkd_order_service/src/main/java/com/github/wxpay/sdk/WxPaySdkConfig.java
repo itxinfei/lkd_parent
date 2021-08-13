d42e7af1ba16f9f955f0d32391d145e386389259
pangzhao <747011882@qq.com>
2021-08-13 19:26:17 +0800

    更新错误，后期添加页码

--- lkd_service/lkd_order_service/src/main/java/com/github/wxpay/sdk/WxPaySdkConfig.java
+++ lkd_service/lkd_order_service/src/main/java/com/github/wxpay/sdk/WxPaySdkConfig.java
@@ -6,35 +6,51 @@ import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 
+/**
+ * @author Administrator
+ */
 @Component
 public class WxPaySdkConfig extends WXPayConfig {
+
     @Autowired
     private WXConfig wxConfig;
 
+    @Override
     public String getAppID() {
         return wxConfig.getAppId();
     }
 
+    @Override
+    public WXPayConstants.SignType getSignType() {
+        return null;
+    }
+
+    @Override
     public String getMchID() {
         return wxConfig.getMchId();
     }
 
+    @Override
     public String getKey() {
         return wxConfig.getPartnerKey();
     }
 
-    InputStream getCertStream() {
+    @Override
+    public InputStream getCertStream() {
         return null;
     }
 
-    IWXPayDomain getWXPayDomain() {
+    @Override
+    public IWXPayDomain getWXPayDomain() {
         return new IWXPayDomain() {
+            @Override
             public void report(String s, long l, Exception e) {
 
             }
 
+            @Override
             public DomainInfo getDomain(WXPayConfig wxPayConfig) {
-                return new DomainInfo("api.mch.weixin.qq.com",true);
+                return new DomainInfo("api.mch.weixin.qq.com", true);
             }
         };
     }
